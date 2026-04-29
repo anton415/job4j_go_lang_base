9660497de10138e26e92c7773bca67d02608c87e
Anton Serdyuchenko <anton415@gmail.com>
2026-04-29 21:42:10 +0300

    Типы данных | Struct [#505561 #562918]

--- internal/base/validate.go
+++ internal/base/validate.go
@@ -9,8 +9,18 @@ type ValidateRequest struct {
 // Если какое-либо поле пустое, добавляется сообщение об ошибке.
 func Validate(req *ValidateRequest) []string {
 	res := make([]string, 0)
-	if req == nil || req.UserID == "" || req.Title == "" || req.Description == "" {
-		res = append(res, "validate message")
+	if req == nil {
+		res = append(res, "request is nil")
+		return res
+	}
+	if req.UserID == "" {
+		res = append(res, "user id is empty")
+	}
+	if req.Title == "" {
+		res = append(res, "title is empty")
+	}
+	if req.Description == "" {
+		res = append(res, "description is empty")
 	}
 	return res
 }
--- internal/base/validate_test.go
+++ internal/base/validate_test.go
@@ -27,12 +27,52 @@ func Test_Validate(t *testing.T) {
 		t.Parallel()
 
 		req := &base.ValidateRequest{
-			// UserID не заполнен
 			Title:       "title",
 			Description: "description",
 		}
 		res := base.Validate(req)
 
-		assert.Equal(t, []string{"validate message"}, res)
+		assert.Equal(t, []string{"user id is empty"}, res)
+	})
+
+	t.Run("invalid empty title", func(t *testing.T) {
+		t.Parallel()
+
+		req := &base.ValidateRequest{
+			UserID:      "1",
+			Description: "description",
+		}
+		res := base.Validate(req)
+
+		assert.Equal(t, []string{"title is empty"}, res)
+	})
+
+	t.Run("invalid empty description", func(t *testing.T) {
+		t.Parallel()
+
+		req := &base.ValidateRequest{
+			UserID: "1",
+			Title:  "title",
+		}
+		res := base.Validate(req)
+
+		assert.Equal(t, []string{"description is empty"}, res)
+	})
+
+	t.Run("invalid empty fields", func(t *testing.T) {
+		t.Parallel()
+
+		req := &base.ValidateRequest{}
+		res := base.Validate(req)
+
+		assert.Equal(t, []string{"user id is empty", "title is empty", "description is empty"}, res)
+	})
+
+	t.Run("invalid nil request", func(t *testing.T) {
+		t.Parallel()
+
+		res := base.Validate(nil)
+
+		assert.Equal(t, []string{"request is nil"}, res)
 	})
 }
