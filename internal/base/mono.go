3709eb2bb57eaea7929004dbb6edefb4d89c157a
Anton Serdyuchenko <anton415@gmail.com>
2026-04-29 20:00:15 +0300

    Разбор первой программы [#505560 #562770]

--- internal/base/mono.go
+++ internal/base/mono.go
@@ -1,27 +1,10 @@
 package base
 
-// Mono проверяет, является ли последовательность монотонной.
 func Mono(nums []int) bool {
-	// определение направления последовательности
-	direction := 0
+	up, down := true, true
 	for i := 0; i < len(nums)-1; i++ {
-		if nums[i] < nums[i+1] {
-			direction = 1
-			break
-		}
-		if nums[i] > nums[i+1] {
-			direction = -1
-			break
-		}
+		up = up && nums[i] <= nums[i+1]
+		down = down && nums[i] >= nums[i+1]
 	}
-	// валидация последовательности
-	for i := 0; i < len(nums)-1; i++ {
-		if direction == 1 && nums[i] > nums[i+1] {
-			return false
-		}
-		if direction == -1 && nums[i] < nums[i+1] {
-			return false
-		}
-	}
-	return true
+	return up || down
 }
