cfb3187fc9c9c3ec0ee584bbe980030b2d3ddc7c
Anton Serdyuchenko <anton415@gmail.com>
2026-05-11 15:02:28 +0300

    Методы [#505562]

--- internal/base/lru_cache.go
+++ internal/base/lru_cache.go
@@ -20,15 +20,47 @@ func NewLruCache(size int) *LruCache {
 }
 
 func (l *LruCache) Put(key string, value string) {
+	node := &Node{Key: key, Value: value, Prev: l.Head}
+	if l.Head != nil {
+		l.Head.Next = node
+	} else {
+		l.Tail = node
+	}
+	l.Head = node
 
+	count := 1
+	for node := l.Head; node != nil; node = node.Prev {
+		if count == l.size {
+			l.Tail = node
+			node.Prev = nil
+			return
+		}
+		count++
+	}
 }
 
 func (l *LruCache) Get(key string) *string {
-	return nil
+	node := l.find(key)
+	if node == nil {
+		return nil
+	}
+	if node.Next != nil {
+		node.Next.Prev = node.Prev
+		if node.Prev != nil {
+			node.Prev.Next = node.Next
+		} else {
+			l.Tail = node.Next
+		}
+		node.Prev = l.Head
+		node.Next = nil
+		l.Head.Next = node
+		l.Head = node
+	}
+	return &node.Value
 }
 
 func (l *LruCache) find(key string) *Node {
-	for node := l.Head; node != nil; node = node.Next {
+	for node := l.Head; node != nil; node = node.Prev {
 		if node.Key == key {
 			return node
 		}
--- internal/base/lru_cache_test.go
+++ internal/base/lru_cache_test.go
@@ -1,16 +1,69 @@
 package base_test
 
 import (
+	"testing"
+
 	"github.com/stretchr/testify/assert"
 	"job4j.ru/go-lang-base/internal/base"
-	"testing"
 )
 
-func Test_Add(t *testing.T) {
+func Test_Lru_Cache(t *testing.T) {
+	t.Parallel()
 
-	rsl := base.Add(1, 2)
-	expected := 3
+	t.Run("get exists", func(t *testing.T) {
+		t.Parallel()
 
-	assert.Equal(t, rsl, expected)
+		cache := base.NewLruCache(2)
+		cache.Put("1", "one")
 
+		rsl := cache.Get("1")
+
+		assert.Equal(t, "one", *rsl)
+	})
+
+	t.Run("get missing", func(t *testing.T) {
+		t.Parallel()
+
+		cache := base.NewLruCache(2)
+		cache.Put("1", "one")
+
+		rsl := cache.Get("2")
+
+		assert.Nil(t, rsl)
+	})
+
+	t.Run("evicts least recently used", func(t *testing.T) {
+		t.Parallel()
+
+		cache := base.NewLruCache(2)
+		cache.Put("1", "one")
+		cache.Put("2", "two")
+		cache.Put("3", "three")
+
+		rsl1 := cache.Get("1")
+		rsl2 := cache.Get("2")
+		rsl3 := cache.Get("3")
+
+		assert.Nil(t, rsl1)
+		assert.Equal(t, "two", *rsl2)
+		assert.Equal(t, "three", *rsl3)
+	})
+
+	t.Run("get moves item to head", func(t *testing.T) {
+		t.Parallel()
+
+		cache := base.NewLruCache(2)
+		cache.Put("1", "one")
+		cache.Put("2", "two")
+		cache.Get("1")
+		cache.Put("3", "three")
+
+		rsl1 := cache.Get("1")
+		rsl2 := cache.Get("2")
+		rsl3 := cache.Get("3")
+
+		assert.Equal(t, "one", *rsl1)
+		assert.Nil(t, rsl2)
+		assert.Equal(t, "three", *rsl3)
+	})
 }
